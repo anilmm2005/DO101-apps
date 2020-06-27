935f615f9dd155eb202cba5e99e1604b2afd710b
anilmm2005 <anilmm2005@gmail.com>
2020-06-27 17:49:48 +0530

    add saswati end point

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars1!\n');
 });
 
+app.get('/saswati', function (req, res) {
+  res.send('Hello saswati!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
